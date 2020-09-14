1db2e2fbc60126a2a464acbfd36355cf554fbc75
Nicolò Santamaria <nicolo.santamaria@aurigaspa.com>
2020-09-14 15:56:29 +0200

    Bug fix and code cleanup.

--- main.go
+++ main.go
@@ -14,16 +14,12 @@ func main() {
 	}
 
 	for key, dir := range cfg.Dirs {
-		endpoint := fmt.Sprintf("/%s", key)
+		endpoint := fmt.Sprintf("/%s/", key)
 		http.Handle(
 			endpoint,
 			http.StripPrefix(endpoint, http.FileServer(http.Dir(dir.Path))),
 		)
 	}
 
-	// Simple static webserver:
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", cfg.Port), nil))
-	// http.Handle("/sas", http.FileServer(http.Dir("H:\\WorkSpaceNaoto\\OTO_VIEWER\\viewer\\src\\assets\\images")))
-	// log.Fatal(http.ListenAndServe(":8080", nil))
-	// log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("H:\\WorkSpaceNaoto\\OTO_VIEWER\\viewer\\src\\assets\\images"))))
 }
